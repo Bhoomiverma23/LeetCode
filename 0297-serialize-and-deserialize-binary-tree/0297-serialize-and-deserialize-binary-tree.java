8a799120b4c88dd9544e9573bdd3d76e3212663f
Bhoomiverma23 <vermabhoomi183@gmail.com>
2026-06-26 13:47:16 +0530

    Time: 15 ms (50.07%), Space: 47.9 MB (52.16%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            if (curr == null) {
+                sb.append("N,");
+                continue;
+            }
+            sb.append(curr.val).append(",");
+            q.offer(curr.left);
+            q.offer(curr.right);
+        }
+        return sb.toString(); 
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.length() == 0)
+            return null;
+        String[] arr = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int i = 1;
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            if (!arr[i].equals("N")) {
+                curr.left = new TreeNode(Integer.parseInt(arr[i]));
+                q.offer(curr.left);
+            }
+            i++;
+            if (!arr[i].equals("N")) {
+                curr.right = new TreeNode(Integer.parseInt(arr[i]));
+                q.offer(curr.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
